ae9969c2b5c9c5874b8c3cf211131fd3539c3a4a
massa <massafranck@gmail.com>
2018-07-31 14:20:49 +0200

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice1</title>
+</head>
+<body>
+  <?php
+  //Test des paramètres de l'url
+  if(isset($_GET['firstname']) && isset($_GET['lastname'])){
+    //Affichage des valeurs
+    echo  $_GET['firstname']. ' ' . $_GET['lastname'];
+  }
+  ?>
+</body>
+</html>
